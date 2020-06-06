05de6fb11a5491d9d7b27bac44edf0b0c2f611af
chwil <chwil@microsoft.com>
2020-06-06 02:19:33 -0500

    Add Json remove recursioin

--- src/Blog.Web/Startup.cs
+++ src/Blog.Web/Startup.cs
@@ -30,6 +30,12 @@ namespace Blog.Web
             services.AddDbContext<BlogContext>(opt => opt.UseNpgsql(_connectionString));
             services.AddTransient<ISelectRepository<BlogEntry>, BlogEntryRepository>();
             services.AddControllers();
+
+            services.AddMvc()
+                .AddNewtonsoftJson(
+                    options => options.SerializerSettings.ReferenceLoopHandling = 
+                        Newtonsoft.Json.ReferenceLoopHandling.Ignore
+                );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
